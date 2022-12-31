5d46f39e026245a3b7e6322336f266a7fd27bd81
Juarez Sant'Ana Vieira da Cruz <111532056+Juarezvieira@users.noreply.github.com>
2022-12-31 19:33:07 -0300

    I made some progress in the JavaScript code.

--- projeto-05/scripts.js
+++ projeto-05/scripts.js
@@ -1,6 +1,43 @@
-let rate = document.getElementsByTagName("li");
+//card change
+let btn = window.document.querySelector("button");
 
-rate.addEventListener("mouseover", function(){
-    this.removeProperty("backgroundColor");
-    this.style.backgroundColor = "white";
-});
+let rateCard = window.document.querySelector("main");
+
+let thankYouCard = window.document.querySelector("section");
+
+let rate = window.document.querySelector("li");
+
+let thankYouRate = window.document.querySelector("#selection");
+
+function Selection() {
+    rate[1].addEventListener("click", function() {
+        this.style.backgroundColor = " ";
+        this.style.color = " ";
+
+        this.style.backgroundColor = "orange";
+        this.style.color = "white";
+    }
+    )
+};
+
+function cardChange() {
+    btn.addEventListener("click", function () {
+        rateCard.style.display = "none";
+        thankYouCard.style.display = "block";
+    })
+};
+
+function cardChangeCheck() {
+    if (cardChange === true) {
+        true;
+    } else (cardChange === false)
+};
+
+//rate number listener
+
+/*function YouSelected() {
+    if(rate[1] === thankYouRate) {
+        thankYouRate.remove();
+        thankYouRate.innerHTML = "You selected 1 out of 5";
+    }
+}*/
